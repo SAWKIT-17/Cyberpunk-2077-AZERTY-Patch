05248ceaaf8face9e50496829c5a8ebe2405fef5
Nicolas Wieckiewicz <nicolas.jzw@gmail.com>
2023-04-01 15:58:02 +0200

    .EXE Fixed

--- bin/patch.py
+++ bin/patch.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from urllib import request
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import filedialog
@@ -16,12 +17,10 @@ if not os.path.exists(game_path):
         if os.path.exists(game_path):
             break
         else:
-            os.system('cls')
-            messagebox.showerror('Dossier introuvable', 'Le dossier sélectionné ne contient pas le jeu Cyberpunk 2077 !')
-
-os.system('cls')
+            messagebox.showerror('Patch AZERTY - Cyberpunk 2077 by SAWKIT', 'Le dossier sélectionné ne contient pas le jeu Cyberpunk 2077 !')
 
 patch_path = os.path.realpath(__file__)
+print(patch_path)
 for i in range(2):
     patch_path = os.path.dirname(patch_path)
 patch_path = os.path.join(patch_path, "src")
@@ -29,7 +28,19 @@ patch_path = os.path.join(patch_path, "src")
 patch_file = os.path.join(patch_path, 'inputUserMappings.xml')
 origin_file = os.path.join(game_path, 'inputUserMappings.xml')
 
-print(f"Patch \'{patch_file}\' ---> \'{origin_file}\'")
-err_copy = shutil.copyfile(patch_file, origin_file)
-if err_copy == origin_file:
-    messagebox.showinfo('Patch appliqué', 'Le patch a été appliqué avec succès !')
+try:
+    print(f"Patch \'{patch_file}\' ---> \'{origin_file}\'")
+    err_copy = shutil.copyfile(patch_file, origin_file)
+    if err_copy == origin_file:
+        messagebox.showinfo('Patch AZERTY - Cyberpunk 2077 by SAWKIT', 'Le patch a été appliqué avec succès !')
+except FileNotFoundError:
+    url = 'https://github.com/SAWKIT-17/Cyberpunk-2077-mapping-AZERTY-FIX/blob/main/src/inputUserMappings.xml'
+    response = request.urlopen(url)
+    if response.status_code == 200:
+        try:
+            request.urlretrieve(url, patch_file)
+            messagebox.showinfo('Patch AZERTY - Cyberpunk 2077 by SAWKIT', 'Le patch a été appliqué avec succès !')
+        except:
+            messagebox.showerror('Patch AZERTY - Cyberpunk 2077 by SAWKIT', 'ERREUR : Impossible de modifier votre fichier. Vérifiez vos permissions.')
+    else:
+        messagebox.showerror('Patch AZERTY - Cyberpunk 2077 by SAWKIT', 'ERREUR : Impossible de télécharger les fichiers nécessaires. Vérifiez votre connexion internet.')
