f03b8cfde7e5bc3676a18ff754cc53cfc802ea38
Nicolas Wieckiewicz <nicolas.jzw@gmail.com>
2023-04-01 16:38:29 +0200

    .EXE Fixed

--- bin/patch.py
+++ bin/patch.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import shutil
 from urllib import request
 import tkinter as tk
@@ -19,24 +20,23 @@ if not os.path.exists(game_path):
         else:
             messagebox.showerror('Patch AZERTY - Cyberpunk 2077 by SAWKIT', 'Le dossier sélectionné ne contient pas le jeu Cyberpunk 2077 !')
 
-patch_path = os.path.realpath(__file__)
-print(patch_path)
-for i in range(2):
-    patch_path = os.path.dirname(patch_path)
-patch_path = os.path.join(patch_path, "src")
+if getattr(sys, 'frozen', False):
+    patch_path = os.path.dirname(sys.executable)
+else:
+    patch_path = os.path.dirname(os.path.abspath(__file__))
 
 patch_file = os.path.join(patch_path, 'inputUserMappings.xml')
 origin_file = os.path.join(game_path, 'inputUserMappings.xml')
 
 try:
-    print(f"Patch \'{patch_file}\' ---> \'{origin_file}\'")
     err_copy = shutil.copyfile(patch_file, origin_file)
     if err_copy == origin_file:
         messagebox.showinfo('Patch AZERTY - Cyberpunk 2077 by SAWKIT', 'Le patch a été appliqué avec succès !')
 except FileNotFoundError:
-    url = 'https://github.com/SAWKIT-17/Cyberpunk-2077-mapping-AZERTY-FIX/blob/main/src/inputUserMappings.xml'
+    url = 'https://github.com/SAWKIT-17/Cyberpunk-2077-mapping-AZERTY-FIX/blob/f417ef8fc08028b4fd278fe739b519e2dd9ff51f/src/inputUserMappings.xml'
     response = request.urlopen(url)
-    if response.status_code == 200:
+
+    if response.getcode() == 200:
         try:
             request.urlretrieve(url, patch_file)
             messagebox.showinfo('Patch AZERTY - Cyberpunk 2077 by SAWKIT', 'Le patch a été appliqué avec succès !')
